033b5ee11c33fb7a0476e795f55fa68e8b1284af
robert <delahoz.robert@gmail.com>
2024-01-27 11:58:18 -0500

    Initial version of jukeboxPwa generated by generator-jhipster@8.1.0

---
+++ jukebox-pwa/src/main/webapp/app/admin/admin.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+/* jhipster-needle-add-admin-module-import - JHipster will add admin modules imports here */
+
+const routes: Routes = [
+  {
+    path: 'docs',
+    loadComponent: () => import('./docs/docs.component'),
+    title: 'global.menu.admin.apidocs',
+  },
+  /* jhipster-needle-add-admin-route - JHipster will add admin routes here */
+];
+
+export default routes;
