4dc15e63ffbeb75102fff2eea05c1b31ff365b54
robert <delahoz.robert@gmail.com>
2024-04-05 15:39:32 -0400

    media-library - MediaFileAggregate and MediaFileImporter

--- media-library/src/main/java/net/vegatec/media_library/command/aggregates/MediaFileAggregate.java
+++ media-library/src/main/java/net/vegatec/media_library/command/aggregates/MediaFileAggregate.java
@@ -5,9 +5,12 @@ import net.vegatec.media_library.command.commands.CreateMediaFileCommand;
 
 import net.vegatec.media_library.common.events.MediaFileCreatedEvent;
 import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
 import org.axonframework.spring.stereotype.Aggregate;
+
+import java.time.LocalDateTime;
 
 @Aggregate
 @Slf4j
@@ -17,16 +20,37 @@ public class MediaFileAggregate {
     private Long id;
 
     private String path;
-
     private String name;
+    private String sortName;
+    private String artistName;
+    private String artistSortName;
+    private String albumName;
+    private String albumSortName;
 
+    private String albumArtistName;
+    private String albumArtistSortName;
+
+    private Integer albumReleasedOn;
+    private Long playbackLength;
+
+    private LocalDateTime createdOn;
 
     public MediaFileAggregate(){}
 
     @CommandHandler
     public MediaFileAggregate(CreateMediaFileCommand createMediaFileCommand) {
-      //  log.info("CreateAccountCommand received.");
+       // log.info("CreateMediaFileCommand received.");
         AggregateLifecycle.apply(new MediaFileCreatedEvent(
             createMediaFileCommand.getId()));
+    }
+
+
+
+    @EventSourcingHandler
+    public void on(MediaFileCreatedEvent accountCreatedEvent) {
+        //log.info("An MediaFileCreatedEvent occurred.");
+
+
+       // AggregateLifecycle.apply(new MediaFileActivatedEvent(this.accountId, "ACTIVATED"));
     }
 }
